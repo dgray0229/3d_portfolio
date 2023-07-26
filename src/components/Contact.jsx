f8c9396807895200982992003799588dfcbe0cf7
Devin Gray <admin@devingray.io>
2023-07-25 22:53:43 -0700

    finish basic website layout for all components.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
-export default Contact
+const ContactWrapper = () => {
+	const formRef = useRef(null);
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+	const [loading, setLoading] = useState(false);
+
+	const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+	const handleSubmit = (e) => {
+    e.preventDefault();
+        setLoading(true);
+        emailjs
+					.sendForm(
+						"service_61vbyjt",
+						"template_oaudhr3",
+						form,
+						"OeNhn9gENAwReKGGP"
+					)
+					.then(
+						(result) => {
+							console.log(result.text);
+							setLoading(false);
+							setForm({
+								name: "",
+								email: "",
+								message: "",
+							});
+						},
+						(error) => {
+							console.log(error.text);
+							setLoading(false);
+						}
+					);
+  };
+	return (
+		<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+			<motion.div
+				variants={slideIn("left", "tween", 0.2, 1)}
+				className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+			>
+				<p className={`${styles.sectionSubText}`}>Get in touch</p>
+				<h3 className={`${styles.sectionHeadText}`}>Contact</h3>
+				<form
+					ref={formRef}
+					onSubmit={handleSubmit}
+					className="mt-12 flex flex-col gap-8"
+				>
+					<label for="name" className="flex flex-col">
+						<span className="text-white font-medium mb-4">Your Name</span>
+						<input
+							type="text"
+							name="name"
+              placeholder="What's your name?"
+							value={form.name}
+							onChange={handleChange}
+							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+						/>
+					</label>
+					<label for="email" className="flex flex-col">
+						<span className="text-white font-medium mb-4">Your Name</span>
+						<input
+							type="email"
+							name="email"
+              placeholder="What's your email?"
+							value={form.email}
+							onChange={handleChange}
+							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+						/>
+					</label>
+					<label for="message" className="flex flex-col">
+						<span className="text-white font-medium mb-4">Your Name</span>
+						<textarea
+            rows="7"
+							name="message"
+              placeholder="What do you want to say?"
+							value={form.message}
+							onChange={handleChange}
+							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+						/>
+					</label>
+          <button type="submit" className="bg-tertiary py-4 px-6 outline-none w-fit rounded-lg text-white font-bold shadow-md shadow-primary">{loading ? 'Sending...' : 'Send'}</button>
+				</form>
+			</motion.div>
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        <EarthCanvas />
+      </motion.div>
+		</div>
+	);
+};
+
+const Contact = SectionWrapper(ContactWrapper, "contact");
+export default Contact;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
 
-const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-export default Experience
+const ExperienceCard = ({ experience }) => (
+	<VerticalTimelineElement
+		contentStyle={{ background: "#1d1836", color: "#fff" }}
+		contentArrowStyle={{ borderRight: "7px solid  #1d1836" }}
+		date={experience.date}
+		iconStyle={{ background: experience.iconBg }}
+		icon={
+			<div className="flex justify-center items-center w-full h-full">
+				<img
+					src={experience.icon}
+					alt={experience.company_name}
+					className="w-[60%] h-[60%] object-contain"
+				/>
+			</div>
+		}
+	>
+  <div className="text-white text-[24px] font-bold">
+    <h3>{experience.title}</h3>
+    <p className="text-secondary text-[1rem] font-semibold" style={{margin: 0}}>{experience.company_name}</p>
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experience.points.map((desc, index) => (
+        <li key={`experience.point-${index}`} className="text-white-100 text-[14px] pl-1 tracking-wider">{desc}</li>
+      ))}
+    </ul>
+  </div>
+	</VerticalTimelineElement>
+);
+const ExperienceWrapper = () => {
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Experience</p>
+				<h2 className={styles.sectionHeadText}>Work History</h2>
+			</motion.div>
+			<div className="mt-20 flex flex-col">
+				<VerticalTimeline>
+					{experiences.map((experience, index) => (
+						<ExperienceCard key={index} experience={experience} />
+					))}
+				</VerticalTimeline>
+			</div>
+		</>
+	);
+};
+
+const Experience = SectionWrapper(ExperienceWrapper, "experience");
+
+export default Experience;
